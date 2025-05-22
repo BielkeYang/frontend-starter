f18c4194268c1fc71070b18fbc90cea7099b07ea
T. Mjoen <trond@twined.net>
2025-05-22 14:09:42 +0200

    Remove autoprefixer (inc in ecss)

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,7 +1,6 @@
 module.exports = {
   plugins: [
     require('@brandocms/europacss'),
-    require('autoprefixer')({ grid: 'on' }),
     require('postcss-reporter')({
       clearReportedMessages: true,
       throwError: false,
